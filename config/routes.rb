03fb9ff45c3108f8a2947b638102874042888aa3
Edgar Quintana <edgarquintana984@hotmail.com>
2017-01-14 12:40:34 -0500

    Generates Device config, styles and layout with a USER model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   root "dashboard#index"
 
-  resources :expenses, only: [:index]
+  devise_for :users
+  
+  resources :expenses
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
