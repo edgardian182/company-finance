e9ce00916c4009370571d81c3659b072d76ae214
Edgar Quintana <edgarquintana984@hotmail.com>
2017-01-15 06:26:29 -0500

    Create Type and Category Models and bind them in the Expenses New Form Modal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114175026) do
+ActiveRecord::Schema.define(version: 20170115101552) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "expenses", force: :cascade do |t|
     t.string   "concept"
@@ -27,6 +33,12 @@ ActiveRecord::Schema.define(version: 20170114175026) do
   add_index "expenses", ["category_id"], name: "index_expenses_on_category_id"
   add_index "expenses", ["type_id"], name: "index_expenses_on_type_id"
   add_index "expenses", ["user_id"], name: "index_expenses_on_user_id"
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
