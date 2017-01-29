df7c009460f97ae7b97026866a122b9fbbec070e
Edgar Quintana <edgarquintana984@hotmail.com>
2017-01-29 13:31:46 -0500

    Permit Edit and Update a Client via JS

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,6 @@
 class ClientsController < ApplicationController
+  before_action :set_client, only: [:edit,:show,:update,:destroy]
+
   def index
     @tab = :clients
     if current_user.superadmin_role? || current_user.supervisor_role?
@@ -32,11 +34,26 @@ class ClientsController < ApplicationController
   def show
   end
 
+  def update
+    respond_to do |format|
+      if @client.update(client_params)
+        format.html { redirect_to clients_path, notice: 'El cliente ha sido actualizado' }
+        format.js {}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   def destroy
   end
 
   private 
   def client_params
     params.require(:client).permit(:name,:lastname,:address,:phone,:email,:city,:reference,:photo)
+  end
+
+  def set_client
+    @client = Client.find(params[:id])    
   end
 end
