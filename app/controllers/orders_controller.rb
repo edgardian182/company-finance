59521a9033bb51bc60232f16024f71d9729b2c55
Edgar Quintana <edgarquintana984@hotmail.com>
2017-01-29 19:43:23 -0500

    Create orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,23 @@
+class OrdersController < ApplicationController
+  def index
+    @tab = :clients
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
